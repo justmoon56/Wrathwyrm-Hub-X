867dbbb53ed3102d7a3829b718f74db9ec18c709
Moon <arfareza147@gmail.com>
2026-08-07 21:12:31 +0700

    No Update Anymore

--- Overahaul.lua
+++ Overahaul.lua
@@ -1,1 +1,1 @@
-game:GetService("Players").LocalPlayer:Kick("Sorry This Script Can't be load anymore i need team for update it all of path has been patch")
+game:GetService("Players").LocalPlayer:Kick("Closed")
